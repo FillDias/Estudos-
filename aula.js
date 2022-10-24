527aaf038f323bc1fbacf2f0663a1e21beefa0eb
FillDias <79338600+FillDias@users.noreply.github.com>
2022-10-24 20:30:05 -0300

    Atualização

--- aula.js
+++ aula.js
@@ -90,12 +90,25 @@ formula do imc
 imc = peso / (altura * altura)
 */
 
-const peso = 72;
+const peso = 180;
 const altura = 1.70;
 const imc = peso / Math.pow(altura, 2);
 console.log (imc);
 
+if(imc < 18.5){
+    console.log('Abaixo do peso');
 
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('peso normal');
+} else if (imc >= 25 && imc < 30){
+    console.log('acima do peso');
+
+}else if (imc >= 30 && imc < 40){
+    console.log('Obeso');
+
+}else if (imc > 40){
+    console.log('Obesidade Grave');
+}
 
 
 
