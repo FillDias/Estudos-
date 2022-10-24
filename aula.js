b17679068c727864854ff19527cb7070eee5dcfa
FillDias <79338600+FillDias@users.noreply.github.com>
2022-10-24 19:31:58 -0300

    projeto

---
+++ aula.js
@@ -0,0 +1,101 @@
+/*const precocombustivel = 5.79;
+const kmMedioDeLitro = 10;
+const distaciaEmKm = 100;
+
+const litrosConsumidos = distaciaEmKm / kmMedioDeLitro; 
+const valorGasto = litrosConsumidos * precocombustivel;
+console.log(valorGasto);*/
+
+/*const camisetaRenanAzul = true; //verdadeiro
+const camisetalemaoAzul = false; // Falça */
+
+//variaveis e operadores aula 1 video 2
+
+/*const numero = 2;
+
+const numeroPar = (numero % 2) === 0;
+
+if(numeroPar) { 
+    console.log('par');
+
+}
+
+//o simbulo  ! e uma negação  
+if(!numeroPar){
+    console.log('impar');
+
+}
+
+ pode ser usado tambem a linha de cod else{} 
+ exemplo se o numero for impar executa o que esta em if 
+se o numero for par executa o else */
+
+
+/*Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+
+- Preço do etanol;
+- preço da gasolina;
+- O tipo decombustivel que está no seu carro;
+- Gasto médio de combustivel do carro por km;
+- Distância em KM da Viagem; */
+
+/*
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmMedioDeLitro = 10;
+const distanciaEmKM = 100;
+const tipoCombustivel = 'Etanol';
+
+const litrosConsumidos = distanciaEmKM / kmMedioDeLitro;
+
+if(tipoCombustivel === 'Gasolina'){
+
+    const valorGasto = litrosConsumidos * precoEtanol;
+    console.log(valorGasto.toFixed(2));
+
+} else {
+
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
+}
+
+*/
+
+
+////////////////////////////////////////////////////////////
+/*
+const nota1 = 8;
+const nota2 = 7;
+const nota3 = 7;
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(media);
+
+if (media < 5){
+    console.log('Reprovação')
+
+}else if(media >=5 && media <=7){
+    console.log('Recuperação');
+}else{
+    console.log('Passou de Semestre');
+}
+*/
+
+/* 3. o IMC - Indice de massa corporal é um critério da organização 
+mundical da saúdepara dar uma indicação sobre a condição de peso de uma
+pessoa adulta.
+
+formula do imc
+imc = peso / (altura * altura)
+*/
+
+const peso = 72;
+const altura = 1.70;
+const imc = peso / Math.pow(altura, 2);
+console.log (imc);
+
+
+
+
+
